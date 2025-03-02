ccb455e4519dfc035c8b0c8a1cfa6c9832deb18c
Kjoshi04 <2004kritikajoshi@gmail.com>
2025-03-02 13:47:18 +0530

    Initial commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ form.addEventListener('submit',(e)=>{
 });
 const getWordinfo = async(word)=>{
     try{
-    resultDiv.innerHTML = " Fetching data";    
+    resultDiv.innerHTML = " Fetching data...";    
     const response= await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     const data= await response.json();
     let definitions=data[0].meanings[0].definitions[0];
